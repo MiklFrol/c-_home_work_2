f57bf2e7128e7b67cc6f054f375a2477ff8487c5
MiklFrol <miklfrol@mail.ru>
2022-12-24 23:52:08 +0300

    Задача 10 выполнена

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа принимает трехзначное число, и выдает вторую цыфру данного числа
+
+Console.Clear();
+
+Repeat:
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number > 99 && number < 1000)
+{ 
+int tempDigit = number / 10 ;
+int secondDigit = tempDigit % 10;
+Console.WriteLine ($"Для числа {number} второй цифрой является {secondDigit}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+    goto Repeat;
+}
